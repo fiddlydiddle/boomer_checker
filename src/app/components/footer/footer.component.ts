cd313c85bb3dc01934b4dd4588a8a851e632aba8
fiddlydiddle <fiddlydiddle37@gmail.com>
2023-06-18 12:24:28 -0500

    Add sources list and update some data

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ModalComponent } from 'src/app/components/modal/modal.component';
+import { ModalConfig } from 'src/app/datamodels/modal-config.model';
 
 @Component({
   selector: 'app-footer',
   templateUrl: 'footer.component.html',
   styleUrls: ['footer.component.scss']
 })
-export class FooterComponent {}
+export class FooterComponent {
+  @ViewChild('modal') private modalComponent: ModalComponent | undefined;
+  
+  sourcesModalConfig: ModalConfig = {
+    modalTitle: 'Sources',
+    closeButtonLabel: 'Close',
+    hideDismissButton: () => { return true; },
+    onClose: () => {
+      return true;
+    },
+    onDismiss: () => {
+      return true;
+    }
+  };
+
+  public openSourcesModal() {
+    this.modalComponent?.open();
+  }
+}
